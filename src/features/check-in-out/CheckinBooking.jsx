d25ecf455728822e2921ed97887bc94e03b3a51b
Melvin <melvinmps11301@gmail.com>
2025-01-23 02:57:35 +0530

    checkin feature implemented

--- src/features/check-in-out/CheckinBooking.jsx
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -12,6 +12,9 @@ import Spinner from "../../ui/Spinner";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "../bookings/useBooking";
+import { formatCurrency } from "../../utils/helpers";
+import { useCheckin } from "./useCheckin";
+import { useSettings } from "../settings/useSettings";
 
 const Box = styled.div`
   /* Box */
@@ -23,12 +26,16 @@ const Box = styled.div`
 
 function CheckinBooking() {
   const [confirmPaid, setConfirmPaid] = useState(false);
+  const [addBreakfast, setAddBreakfast] = useState(false);
   const { booking = {}, isPending } = useBooking();
+  const { settings = {}, isPending: isSettingsPending } = useSettings();
 
   useEffect(() => {
     setConfirmPaid(booking?.isPaid ?? false);
   }, [booking.isPaid]);
+
   const moveBack = useMoveBack();
+  const { checkIn, isCheckingin } = useCheckin();
 
   const {
     id: bookingId,
@@ -39,9 +46,26 @@ function CheckinBooking() {
     numNights,
   } = booking;
 
-  function handleCheckin() {}
+  function handleCheckin() {
+    if (!confirmPaid) return;
 
-  if (isPending) return <Spinner />;
+    if (addBreakfast) {
+      checkIn({
+        bookingId,
+        breakfast: {
+          hasBreakfast: true,
+          extraPrice: optionalBreakfastPrice,
+          totalPrice: totalPrice + optionalBreakfastPrice,
+        },
+      });
+    } else {
+      checkIn({ bookingId, breakfast: {} });
+    }
+  }
+
+  if (isPending || isSettingsPending) return <Spinner />;
+  const optionalBreakfastPrice =
+    settings.breakfastPrice * numNights * numGuests;
 
   return (
     <>
@@ -53,15 +77,41 @@ function CheckinBooking() {
       <BookingDataBox booking={booking} />
       <Box>
         <Checkbox
-          value={confirmPaid}
+          checked={confirmPaid}
+          disabled={confirmPaid || isCheckingin}
           onChange={() => setConfirmPaid((s) => !s)}
           id="confirm"
         >
-          I Confirm that the {guests.fullName} has paid the full amount
+          I Confirm that the {guests.fullName} has paid the total amount of{" "}
+          {!addBreakfast
+            ? formatCurrency(totalPrice)
+            : `${formatCurrency(
+                optionalBreakfastPrice + totalPrice
+              )} (Total Price @ ${formatCurrency(
+                totalPrice
+              )} + BreakFast @ ${formatCurrency(optionalBreakfastPrice)})`}
         </Checkbox>
       </Box>
+      {!hasBreakfast && (
+        <Box>
+          {/* {isSettingsPending && <SpinnerMini />} */}
+          <Checkbox
+            checked={addBreakfast}
+            // disabled={addBreakfast || isCheckingin}
+            onChange={() => {
+              setAddBreakfast((s) => !s);
+              setConfirmPaid(false);
+            }}
+            id="breakfast"
+          >
+            Want to add Breakfast for {formatCurrency(optionalBreakfastPrice)}?
+          </Checkbox>
+        </Box>
+      )}
       <ButtonGroup>
-        <Button onClick={handleCheckin}>Check in booking #{bookingId}</Button>
+        <Button onClick={handleCheckin} disabled={!confirmPaid || isCheckingin}>
+          Check in booking #{bookingId}
+        </Button>
         <Button $variation="secondary" onClick={moveBack}>
           Back
         </Button>
