f2a85c9acda33f857c22f04e5f0bbb08b501237d
Melvin <melvinmps11301@gmail.com>
2025-01-23 17:37:05 +0530

    ready for first deployment

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("dev@dev.dev");
-  const [password, setPassword] = useState("dev1234");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isPending } = useLogin();
 
   function handleSubmit(e) {
