29bf60fab333302d2ca519bd54f9248f3bc6c8f8
Melvin <melvinmps11301@gmail.com>
2025-01-23 14:20:43 +0530

    setting up dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>TodaysActivities</div>
+      <div>SalesChart</div>
+      <div>DurationChart</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
